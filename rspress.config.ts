898ed7851031a67e4a3fce93219a986ac11f7af4
gitychzh <93921526@163.com>
2025-12-27 21:50:10 +0800

    444

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,10 +2,10 @@ import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),//推荐
-  // root: 'docs',
-  outDir: path.join(__dirname, 'jsj'),//推荐
-  // outDir: 'jsj',
+  // root: path.join(__dirname, 'docs'),//推荐
+  root: 'docs',
+  // outDir: path.join(__dirname, 'jsj'),//推荐
+  outDir: 'jsj',
   base: '/jsj/',
   title: '红薯课堂_计算机',
   icon: '/logo_红薯课堂.png',
