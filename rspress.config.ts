fe3f1242cbf9eec79bb9c5a13e6592f5ba4b445f
gitychzh <93921526@163.com>
2025-12-28 21:49:37 +0800

    lang en

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,6 +2,8 @@ import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+  // lang: 'zh',
+  lang: 'en',
   // root: path.join(__dirname, 'docs'),//推荐,但是改了之后docs/_meta.json失效了
   root: 'docs',
   // outDir: path.join(__dirname, 'jsj'),//推荐，但是改了之后docs/_meta.json失效了
